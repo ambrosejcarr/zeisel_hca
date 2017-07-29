3e43c929b589253b7ee5dd5d987e5a558e43bb3d
Ambrose J Carr <mail@ambrosejcarr.com>
2017-07-29 11:22:23 -0400

    removed script with server information

--- src/zeisel/sync.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-# simple script to sync this repository to the server I am currently using to process the data.
-
-rsync -Pav -e "ssh -i ${AWS_RSA_KEY}" ~/projects/zeisel_hca ec2-user@54.210.155.150:/home/ec2-user
